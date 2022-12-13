49483ec69e69f62b3fe292a717c8a2fe8c213ea6
Bartłomiej Rudecki <ejbtrd@gmail.com>
2022-12-13 16:05:18 +0100

    sepolicy: Initialize tequila
    Change-Id: Id87d662ff71f47514f5671bb90f439c5b8319aea

--- atv/sepolicy.mk
+++ atv/sepolicy.mk
@@ -5,8 +5,8 @@
 
 ifneq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 BOARD_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/atv/vendor
+    device/tequila/sepolicy/atv/vendor
 endif
 
 PRODUCT_PRIVATE_SEPOLICY_DIRS += \
-   device/lineage/sepolicy/atv/private
+   device/tequila/sepolicy/atv/private
