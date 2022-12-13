182a65ccfa0100b70f267554c3a4e2daabc659ba
Bartłomiej Rudecki <ejbtrd@gmail.com>
2022-12-13 16:05:18 +0100

    sepolicy: tequila
    Change-Id: Id87d662ff71f47514f5671bb90f439c5b8319aea

--- atv/sepolicy.mk
+++ atv/sepolicy.mk
@@ -1,12 +1,12 @@
 #
 # This policy configuration will be used by atv products that
-# inherit from Lineage
+# inherit from tequila
 #
 
 ifneq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 BOARD_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/atv/vendor
+    device/tequila/sepolicy/atv/vendor
 endif
 
 PRODUCT_PRIVATE_SEPOLICY_DIRS += \
-   device/lineage/sepolicy/atv/private
+   device/tequila/sepolicy/atv/private
--- exynos/sepolicy.mk
+++ exynos/sepolicy.mk
@@ -10,14 +10,14 @@ endif
 endif
 
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/exynos/private
+    device/tequila/sepolicy/exynos/private
 
 ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/exynos/dynamic \
-    device/lineage/sepolicy/exynos/system
+    device/tequila/sepolicy/exynos/dynamic \
+    device/tequila/sepolicy/exynos/system
 else
 BOARD_VENDOR_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/exynos/dynamic \
-    device/lineage/sepolicy/exynos/vendor
+    device/tequila/sepolicy/exynos/dynamic \
+    device/tequila/sepolicy/exynos/vendor
 endif
